dd239a010decdec38fbc64c5fd02ee706690ea1d
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-15 17:14:49 -0300

    Sass

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -1,11 +1,12 @@
 import React from "react"
 import { NavLink } from "react-router-dom"
+import styles from './_NavBar.module.scss';
 
 
 const NavBar: React.FC = () => {
 
   return (
-    <div>
+    <div className={styles.navBar}>
       <NavLink to="/products">Products</NavLink>
       <NavLink to="/signup">Sign Up</NavLink>
       <NavLink to="/login">Log in</NavLink>
