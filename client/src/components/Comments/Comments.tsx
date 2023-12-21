2c0523b9afb21642b488f5c05e3fe154ddc69fee
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-21 16:14:56 -0300

    Normalize variables

--- client/src/components/Comments/Comments.tsx
+++ client/src/components/Comments/Comments.tsx
@@ -3,7 +3,8 @@ import styles from './_Comments.module.scss';
 const Comments = () => {
   return (
     <div className={styles.container}>
-      <h1>Comments</h1>
+      <h1>Condiciones de suelo</h1>
+      <p>Existen diferentes condociones de suelo y medio ambiente que se pueden presentar a la hora de realizar un proceso de producción agrícola. En ciertos casos esas condiciones resultan inapropiadas para la obtención de los rendimientos óptimos en los cultivos. Nuestro producto ofrece una solución efectiva para mejorar las condiciones del suelo con especial énfasis en aquellos que se requiere aumentar el nivel de ácidos húmicos para logara un mejor equilibrio del suelo. </p>
     </div>
   )
 }
