2c152f27d9d359958fd05f5c6fd145672f6216fd
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-22 18:53:26 -0300

    app + index

---
+++ api/src/app.ts
@@ -0,0 +1,26 @@
+import express from 'express';
+import morgan from 'morgan';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+const port = process.env.PORT || 3001;
+
+// initializations
+const app = express();
+
+// settings
+app.set('port', port); 
+
+// middlewares
+app.use(morgan('dev'));
+app.use(cors());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+
+// routes
+app.get('/', (req, res) => {
+    res.send(`the api is at http://localhost:${port}`);
+});
+
+export default app;
