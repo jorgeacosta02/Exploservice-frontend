5b8ea5ec190bda0ca1c4b4646e4076c399d684d6
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-23 21:32:41 -0300

    postContactMe controller

---
+++ api/src/controllers/postContactMe.ts
@@ -0,0 +1,134 @@
+import { Request, Response } from 'express';
+const nodemailer = require("nodemailer");
+import dotenv from "dotenv";
+dotenv.config();
+const { NODEMAILER_USER, NODEMAILER_PASS, DESTINATION_EMAIL } = process.env;
+
+const postContactMe = async (req: Request, res: Response) => {
+  try {
+    const {name,email,subject,message} = req.body;
+    // inicia la funcion de recibir el mensaje
+    let transporter = nodemailer.createTransport({
+      //options -- define los datos de conexión
+      host: "smtp.gmail.com",
+      port: 587,
+      secure: false,
+      auth: {
+        user: NODEMAILER_USER,
+        pass: NODEMAILER_PASS,
+      },
+    });
+    // se fusionará en cada objeto de mensaje.
+    let mailOptions = {
+      from: NODEMAILER_USER,
+      to: DESTINATION_EMAIL,
+      subject,
+      html: `<html>
+	<head>
+        <body>
+        <h3 style="color:#9E7842">Datos del usuario:<h3>
+        <p><b>Nombre:</b> ${name}.</p>
+        <p><b>Correo electrónico:</b> ${email}</p>
+        </br>
+        <h3 style="color:#9E7842">Solicitud:</h3>
+        <p>🍻 ${message}.<p>
+		</body>
+	</head>
+</html>`,
+    };
+
+    transporter.sendMail(mailOptions, (error: Error, info:string) => {
+      if (error) {
+        return res.status(500).send(error.message)
+      } else {
+       return res.status(200).send("it was sent satisfactorily")
+      }
+    });
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error.message);
+     return res.status(500).send(error.message)
+    } else {
+      console.log("Unexpected error");
+      return res.status(500).send("Unexpected error")
+    }
+  }
+};
+
+export default postContactMe;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { Request, Response } from 'express';
+// import nodemailer from 'nodemailer';
+// import dotenv from "dotenv";
+// dotenv.config();
+// const { NODEMAILER_USER, NODEMAILER_PASS, DESTINATION_EMAIL } = process.env;
+
+// const postContactMe = async (req: Request, res: Response) => {
+//   try {
+//     const { name, email, subject, message } = req.body;
+//     // inicia la funcion de recibir el mensaje
+//     let transporter = nodemailer.createTransport({
+//       //options -- define los datos de conexión
+//       host: "smtp.gmail.com",
+//       port: 587,
+//       secure: false,
+//       auth: {
+//         user: NODEMAILER_USER,
+//         pass: NODEMAILER_PASS,
+//       },
+//     });
+//     // se fusionará en cada objeto de mensaje.
+//     let mailOptions = {
+//       from: NODEMAILER_USER,
+//       to: DESTINATION_EMAIL,
+//       subject,
+//       html: `<html>
+// 	<head>
+//         <body>
+//         <h3 style="color:#9E7842">Datos del usuario:<h3>
+//         <p><b>Nombre:</b> ${name}.</p>
+//         <p><b>Correo electrónico:</b> ${email}</p>
+//         </br>
+//         <h3 style="color:#9E7842">Solicitud:</h3>
+//         <p>🍻 ${message}.<p>
+// 		</body>
+// 	</head>
+// </html>`,
+//     };
+
+//     // transporter.sendMail(mailOptions, (error: Error, info:string) => {
+//      transporter.sendMail(mailOptions, (error: Error | null, info: nodemailer.SentMessageInfo) => void {
+
+//       if (error: any) {
+//         return res.status(500).send(error.message)
+//       } else {
+//        return res.status(200).send("it was sent satisfactorily")
+//       }
+//     });
+//   } catch (error) {
+//     if (error instanceof Error) {
+//       console.log(error.message);
+//      return res.status(500).send(error.message)
+//     } else {
+//       console.log("Unexpected error");
+//       return res.status(500).send("Unexpected error")
+//     }
+//   }
+// };
+
+// export default postContactMe;
