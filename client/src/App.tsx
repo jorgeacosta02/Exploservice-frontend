da9673009fe08ea3ce569fb08104cec168cd186e
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-24 16:02:20 -0300

    Duplicate directory

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,17 +1,28 @@
 import { Routes, Route } from 'react-router-dom';
-import Home from './pages/Home/Home';
-import Product from './pages/Product/Product';
-import Company from './pages/Company/Company';
-import Contact from './pages/Contact/Contact';
+import Home from './pages/Exploagro/Home/Home';
+import Product from './pages/Exploagro/Product/Product';
+import Company from './pages/Exploagro/Company/Company';
+import Contact from './pages/Exploagro/Contact/Contact';
 
 const App = () => {
   return (
     <div>
       <Routes>
+
+        {/*General Routes*/}
         <Route path="/" element={<Home />} />
-        <Route path="/product" element={<Product />} />
-        <Route path="/company" element={<Company />} />
-        <Route path="/contact" element={<Contact />} />
+
+        {/*Exploagro Routes*/}
+        <Route path="/exploagro/" element={<Home />} />
+        <Route path="/exploagro/product" element={<Product />} />
+        <Route path="/exploagro/company" element={<Company />} />
+        <Route path="/exploagro/contact" element={<Contact />} />
+
+        {/*Exploservice Routes*/}
+        <Route path="/exploservice/" element={<Home />} />
+        <Route path="/exploservice/product" element={<Product />} />
+        <Route path="/exploservice/company" element={<Company />} />
+        <Route path="/exploservice/contact" element={<Contact />} />
       </Routes>
     </div>
   )
