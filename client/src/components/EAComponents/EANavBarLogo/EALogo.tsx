10f261e58ddffc019aea50bd8ae3882be9cf690f
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-25 14:02:35 -0300

    Color styles

--- client/src/components/EAComponents/EANavBarLogo/EALogo.tsx
+++ client/src/components/EAComponents/EANavBarLogo/EALogo.tsx
@@ -3,7 +3,7 @@ import styles from './_EALogo.module.scss';
 const EALogo = () => {
   return (
     <div className={styles.logo}>
-      <h1 className={styles.text}>ExploAgro</h1>
+      <h1 className={styles.text}>EXPLOAGRO</h1>
     </div>
   )
 }
