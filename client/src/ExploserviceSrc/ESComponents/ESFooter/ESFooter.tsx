8f93a472083809ca45ff0465b915ff5fdd6e8acf
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-27 09:56:59 -0300

    Exploservice footer

---
+++ client/src/ExploserviceSrc/ESComponents/ESFooter/ESFooter.tsx
@@ -0,0 +1,13 @@
+import styles from './_ESFooter.module.scss';
+
+const ESFooter = () => {
+  return (
+    <div className={styles.container}>
+      <p className={styles.text}>
+        Exploservice &reg; | Todos los derechos reservados | Diseño Web Jorge Acosta
+      </p>
+    </div>
+  )
+}
+
+export default ESFooter
--- client/src/ExploserviceSrc/ESPages/ESHome/ESHome.tsx
+++ client/src/ExploserviceSrc/ESPages/ESHome/ESHome.tsx
@@ -1,11 +1,13 @@
 import ESComments from '../../ESComponents/ESComments/ESComments'
 import ESNavBar from '../../ESLayouts/ESNavBar/ESNavBar'
+import ESFooter from '../../ESComponents/ESFooter/ESFooter'
 
 const ESHome = () => {
   return (
     <div>
       <ESNavBar />
       <ESComments />
+      <ESFooter />
     </div>
   )
 }
