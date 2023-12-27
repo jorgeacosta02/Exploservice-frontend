4108dfe3d896e748d3b2ce2ed05087ab470d2d84
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-27 17:49:03 -0300

    form and multer settings

--- api/src/app.ts
+++ api/src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import cors from 'cors';
+import multer from 'multer';
 import authRoutes from './routes/auth.routes';
 import passport from 'passport';
 import passportMiddleware from './middlewares/passport';
@@ -13,6 +14,10 @@ const port = process.env.PORT || 3001;
 
 // initializations
 const app = express();
+
+// Multer settings
+const storage = multer.memoryStorage();
+const upload = multer({ storage });
 
 // settings
 app.set('port', port); 
@@ -29,6 +34,18 @@ passport.use(passportMiddleware)
 app.get('/', (req, res) => {
     res.send(`the api is at http://localhost:${port}`);
 });
+
+// Ruta para manejar la carga de imágenes
+app.post('/api/upload', upload.array('images', 5), (req, res) => {
+    // req.files contiene la información de las imágenes cargadas por Multer
+    const images = req.files as Express.Multer.File[];
+  
+    // Aquí puedes procesar las imágenes y guardar las URL en la base de datos
+    // Por ejemplo, puedes utilizar Cloudinary para almacenar las imágenes y obtener las URL
+  
+    // Respuesta de ejemplo
+    res.json({ message: 'Imágenes cargadas exitosamente', images });
+  });
 
 app.use(authRoutes);
 app.use(specialRoutes);
