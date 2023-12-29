d21f3cf84a00d7a83202db5aad3aec37a4187dfc
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-28 22:26:42 -0300

    ESServicesFormComp.tsx

--- api/src/controllers/ESControllers/ESImgUloadController.ts
+++ api/src/controllers/ESControllers/ESImgUloadController.ts
@@ -1,31 +1,33 @@
 // servicio.controller.ts
 import { Request, Response } from 'express';
-import cloudinary from '../../config/cloudinary'; // Ajusta la ruta según tu estructura
+// import cloudinary from '../../config/cloudinary'; // Ajusta la ruta según tu estructura
 import Servicio from '../../models/ESModels/ESImgUlodaModel'; // Ajusta la ruta según tu estructura
 
 // Controlador para manejar la carga de imágenes y guardar en MongoDB
 export const ESImgUloadController = async (req: Request, res: Response) => {
     try {
-        // Obtener imágenes desde Multer
-        const images = req.files as Express.Multer.File[];
 
-        // Subir imágenes a Cloudinary
-        const uploadedImageUrls = await Promise.all(
-            images.map(async (image) => {
-                const result = await cloudinary.uploader.upload(image.buffer.toString('base64'));
-                return result.url;
-            })
-        );
+
+        // // Obtener imágenes desde Multer
+        // const images = req.files as Express.Multer.File[];
+
+        // // Subir imágenes a Cloudinary
+        // const uploadedImageUrls = await Promise.all(
+        //     images.map(async (image) => {
+        //         const result = await cloudinary.uploader.upload(image.buffer.toString('base64'));
+        //         return result.url;
+        //     })
+        // );
 
         // Crear un nuevo servicio con datos y URLs de imágenes
-        const nuevoServicio = new Servicio({
+        const newService = new Servicio({
             title: req.body.title,
             description: req.body.description,
-            imageUrls: uploadedImageUrls,
+            imageUrls: req.body.images,
         });
 
         // Guardar el servicio en MongoDB
-        await nuevoServicio.save();
+        await newService.save();
 
         res.json({ message: 'Imágenes cargadas y servicio guardado exitosamente' });
     } catch (error) {
--- client/src/ExploserviceSrc/ESComponents/ESServicesFormComp/ESServicesFormComp.tsx
+++ client/src/ExploserviceSrc/ESComponents/ESServicesFormComp/ESServicesFormComp.tsx
@@ -1,98 +1,94 @@
 import { useState } from 'react';
 import { useDrop } from 'react-dnd';
-// import { HTML5Backend } from 'react-dnd-html5-backend';
 import ImageUploading, { ImageListType } from 'react-images-uploading';
 import axios from 'axios';
 import styles from './_ESServicesFormComp.module.scss';
 
 const ESServicesFormComp = () => {
-  // Estado para almacenar la lista de imágenes seleccionadas
   const [images, setImages] = useState<ImageListType>([]);
-
-  // Estados para el título y la descripción del formulario
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
 
-  // Hook de react-dnd para habilitar la funcionalidad de arrastrar y soltar
   const [, drop] = useDrop({
-    accept: 'image', // Acepta elementos arrastrables con el tipo 'image'
-    drop: (item: any) => handleDrop(item), // Llama a la función handleDrop cuando se suelta un elemento
+    accept: 'image',
+    drop: (item: any) => handleDrop(item),
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
       canDrop: !!monitor.canDrop(),
     }),
-    options: { dropEffect: 'copy' }, // Indica que se debe copiar el elemento
+    options: { dropEffect: 'copy' },
   });
 
-  // Función para manejar el evento de soltar una imagen
-  const handleDrop = (item: any) => {
-      // Crea una copia del objeto de la imagen
-    const copiedImage = Object.assign({}, item);
+  const handleDrop = async (item: any) => {
+    try {
+      const formData = new FormData();
+      formData.append('file', item.file);
 
-     // Agrega la imagen copiada a la lista de imágenes seleccionadas
-    setImages((prevImages) => [...prevImages, { file: copiedImage.file }])
+      const cloudinaryResponse = await axios.post(
+        'https://api.cloudinary.com/v1_1/tu-cloud-name/image/upload',
+        formData,
+        {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        }
+      );
 
+      const imageUrl = cloudinaryResponse.data.secure_url;
 
-
-
-    // // Agrega la imagen a la lista de imágenes seleccionadas
-    // setImages((prevImages) => [...prevImages, { file: item.file }]);
+      setImages((prevImages) => [...prevImages, { dataURL: imageUrl }]);
+    } catch (error) {
+      console.error('Error al cargar la imagen a Cloudinary:', error);
+    }
   };
 
-  // Función para manejar la eliminación de una imagen de la lista
   const handleRemove = (index: number) => {
     setImages((prevImages) => prevImages.filter((_, i) => i !== index));
   };
 
-  // Función para manejar el envío del formulario
   const handleSubmit = async () => {
     try {
-      // Crea un objeto FormData para enviar datos al servidor
-      const formData = new FormData();
+      const uploadPromises = images.map(async (image) => {
+        const response = await axios.post(
+          'https://api.cloudinary.com/v1_1/tu-cloud-name/image/upload',
+          { file: image.dataURL },
+          {
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          }
+        );
 
-      // Agrega el título y la descripción al FormData
-      formData.append('title', title);
-      formData.append('description', description);
-
-      // Itera sobre la lista de imágenes seleccionadas
-      images.forEach((image, index) => {
-        // Verifica que la propiedad file de la imagen no sea undefined
-        if (image.file) {
-          // Agrega la imagen al FormData, usando el índice para diferenciarlas
-          formData.append(`images[${index}]`, image.file);
-        }
+        return response.data.secure_url;
       });
 
-      // Realiza una solicitud POST al servidor con Axios
-      const response = await axios.post('/api/upload', formData, {
-        headers: {
-          'Content-Type': 'multipart/form-data', // Especifica el tipo de contenido como multipart/form-data
-        },
-      });
+      const uploadedImageUrls = await Promise.all(uploadPromises);
 
-      // Maneja la respuesta del servidor (puede mostrar un mensaje de éxito, etc.)
+      const formData = {
+        title,
+        description,
+        images: uploadedImageUrls,
+      };
+
+      const response = await axios.post('/exploservice/upload', formData);
+
       console.log('Respuesta del servidor:', response.data);
 
-      // Limpia el formulario después de la carga exitosa
       setImages([]);
       setTitle('');
       setDescription('');
     } catch (error) {
-      // Maneja los errores de carga
       console.error('Error al enviar formulario:', error);
     }
   };
 
-  // Renderiza el componente del formulario
   return (
     <div ref={drop}>
       <div className={styles.container}>
         <h1>AGREGAR SERVICIO</h1>
         <div className={styles.dataContainer}>
-
           <div className={styles.textContainer}>
             <div className={styles.inputBlock}>
-              {/* Input para el título del formulario */}
               <label>Nombre:</label>
               <input
                 type="text"
@@ -102,7 +98,6 @@ const ESServicesFormComp = () => {
               />
             </div>
             <div className={styles.inputBlock}>
-              {/* Textarea para la descripción del formulario */}
               <label>Descripción:</label>
               <textarea
                 value={description}
@@ -111,37 +106,29 @@ const ESServicesFormComp = () => {
               />
             </div>
           </div>
-          {/* Componente ImageUploading para manejar la carga de imágenes */}
           <ImageUploading
             multiple
             value={images}
             onChange={(imageList) => setImages(imageList)}
             maxNumber={6}
-            >
+          >
             {({ onImageUpload }) => (
               <div className={styles.mediaContainer}>
-                {/* Botón para iniciar la carga de imágenes */}
-                <button
-                  onClick={onImageUpload}
-                  className={styles.imgButton}
-                  >
+                <button onClick={onImageUpload} className={styles.imgButton}>
                   Seleccionar Imágenes
                 </button>
-                {/* Mapea sobre la lista de imágenes seleccionadas */}
                 <div className={styles.imgContainer}>
                   {images.map((image, index) => (
-                    <div className={styles.imgBlock} key= {index}
-                    >
+                    <div className={styles.imgBlock} key={index}>
                       {/* Muestra la imagen seleccionada */}
                       <img
                         src={image.dataURL}
                         alt={`img-${index}`}
-                        />
-                      {/* Botón para quitar la imagen de la lista */}
+                      />
                       <button
                         onClick={() => handleRemove(index)}
                         className={styles.xButton}
-                        >
+                      >
                         X
                       </button>
                     </div>
@@ -151,11 +138,7 @@ const ESServicesFormComp = () => {
             )}
           </ImageUploading>
         </div>
-        {/* Botón para enviar el formulario */}
-        <button
-          onClick={handleSubmit}
-          className={styles.imgButton}
-          >
+        <button onClick={handleSubmit} className={styles.imgButton}>
           Cargar Servicio
         </button>
       </div>
@@ -164,3 +147,185 @@ const ESServicesFormComp = () => {
 };
 
 export default ESServicesFormComp;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { useState } from 'react';
+// import { useDrop } from 'react-dnd';
+// // import { HTML5Backend } from 'react-dnd-html5-backend';
+// import ImageUploading, { ImageListType } from 'react-images-uploading';
+// import axios from 'axios';
+// import styles from './_ESServicesFormComp.module.scss';
+
+// const ESServicesFormComp = () => {
+//   // Estado para almacenar la lista de imágenes seleccionadas
+//   const [images, setImages] = useState<ImageListType>([]);
+
+//   // Estados para el título y la descripción del formulario
+//   const [title, setTitle] = useState('');
+//   const [description, setDescription] = useState('');
+
+//   // Hook de react-dnd para habilitar la funcionalidad de arrastrar y soltar
+//   const [, drop] = useDrop({
+//     accept: 'image', // Acepta elementos arrastrables con el tipo 'image'
+//     drop: (item: any) => handleDrop(item), // Llama a la función handleDrop cuando se suelta un elemento
+//     collect: (monitor) => ({
+//       isOver: !!monitor.isOver(),
+//       canDrop: !!monitor.canDrop(),
+//     }),
+//     options: { dropEffect: 'copy' }, // Indica que se debe copiar el elemento
+//   });
+
+//   // Función para manejar el evento de soltar una imagen
+//   const handleDrop = (item: any) => {
+//       // Crea una copia del objeto de la imagen
+//     const copiedImage = Object.assign({}, item);
+
+//      // Agrega la imagen copiada a la lista de imágenes seleccionadas
+//     setImages((prevImages) => [...prevImages, { file: copiedImage.file }])
+
+
+
+
+//     // // Agrega la imagen a la lista de imágenes seleccionadas
+//     // setImages((prevImages) => [...prevImages, { file: item.file }]);
+//   };
+
+//   // Función para manejar la eliminación de una imagen de la lista
+//   const handleRemove = (index: number) => {
+//     setImages((prevImages) => prevImages.filter((_, i) => i !== index));
+//   };
+
+//   // Función para manejar el envío del formulario
+//   const handleSubmit = async () => {
+//     try {
+//       // Crea un objeto FormData para enviar datos al servidor
+//       const formData = new FormData();
+
+//       // Agrega el título y la descripción al FormData
+//       formData.append('title', title);
+//       formData.append('description', description);
+
+//       // Itera sobre la lista de imágenes seleccionadas
+//       images.forEach((image, index) => {
+//         // Verifica que la propiedad file de la imagen no sea undefined
+//         if (image.file) {
+//           // Agrega la imagen al FormData, usando el índice para diferenciarlas
+//           formData.append(`images[${index}]`, image.file);
+//         }
+//       });
+
+//       // Realiza una solicitud POST al servidor con Axios
+//       const response = await axios.post('/exploservice/upload', formData, {
+//         headers: {
+//           'Content-Type': 'multipart/form-data', // Especifica el tipo de contenido como multipart/form-data
+//         },
+//       });
+
+//       // Maneja la respuesta del servidor (puede mostrar un mensaje de éxito, etc.)
+//       console.log('Respuesta del servidor:', response.data);
+
+//       // Limpia el formulario después de la carga exitosa
+//       setImages([]);
+//       setTitle('');
+//       setDescription('');
+//     } catch (error) {
+//       // Maneja los errores de carga
+//       console.error('Error al enviar formulario:', error);
+//     }
+//   };
+
+//   // Renderiza el componente del formulario
+//   return (
+//     <div ref={drop}>
+//       <div className={styles.container}>
+//         <h1>AGREGAR SERVICIO</h1>
+//         <div className={styles.dataContainer}>
+//           <div className={styles.textContainer}>
+//             <div className={styles.inputBlock}>
+//               {/* Input para el título del formulario */}
+//               <label>Nombre:</label>
+//               <input
+//                 type="text"
+//                 value={title}
+//                 placeholder='Ingrese nombre del servicio...'
+//                 onChange={(e) => setTitle(e.target.value)}
+//               />
+//             </div>
+//             <div className={styles.inputBlock}>
+//               {/* Textarea para la descripción del formulario */}
+//               <label>Descripción:</label>
+//               <textarea
+//                 value={description}
+//                 placeholder='Ingrese descripción del servicio...'
+//                 onChange={(e) => setDescription(e.target.value)}
+//               />
+//             </div>
+//           </div>
+//           {/* Componente ImageUploading para manejar la carga de imágenes */}
+//           <ImageUploading
+//             multiple
+//             value={images}
+//             onChange={(imageList) => setImages(imageList)}
+//             maxNumber={6}
+//             >
+//             {({ onImageUpload }) => (
+//               <div className={styles.mediaContainer}>
+//                 {/* Botón para iniciar la carga de imágenes */}
+//                 <button
+//                   onClick={onImageUpload}
+//                   className={styles.imgButton}
+//                   >
+//                   Seleccionar Imágenes
+//                 </button>
+//                 {/* Mapea sobre la lista de imágenes seleccionadas */}
+//                 <div className={styles.imgContainer}>
+//                   {images.map((image, index) => (
+//                     <div className={styles.imgBlock} key= {index}
+//                     >
+//                       {/* Muestra la imagen seleccionada */}
+//                       <img
+//                         src={image.dataURL}
+//                         alt={`img-${index}`}
+//                         />
+//                       {/* Botón para quitar la imagen de la lista */}
+//                       <button
+//                         onClick={() => handleRemove(index)}
+//                         className={styles.xButton}
+//                         >
+//                         X
+//                       </button>
+//                     </div>
+//                   ))}
+//                 </div>
+//               </div>
+//             )}
+//           </ImageUploading>
+//         </div>
+//         {/* Botón para enviar el formulario */}
+//         <button
+//           onClick={handleSubmit}
+//           className={styles.imgButton}
+//           >
+//           Cargar Servicio
+//         </button>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ESServicesFormComp;
