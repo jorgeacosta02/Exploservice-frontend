f356c3f0a6b51733dcc92a3e0db3ef3b94e3faaa
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2023-12-31 20:22:46 -0300

    ES styles

--- client/src/ExploserviceSrc/ESComponents/ESCommentsComp/ESCommentsComp.tsx
+++ client/src/ExploserviceSrc/ESComponents/ESCommentsComp/ESCommentsComp.tsx
@@ -17,13 +17,18 @@ import imgPD03 from '../../ESAssets/ESImages/ESServices/PreDrilling/preDrilling0
 const ESCommentsComp = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.mainImage}>
+      </div>
+      <div className={styles.mainText}>
+
       <h1 className={styles.title}>EXPLOSERVICE - ADL S.R.L.</h1>
-      <p>
-      En Exploservice ADL nos dedicamos a brindar soluciones en el campo de la perforación para voladuras, aplicadas para la exploración y explotación en minería y excavaciones para construcción.
-      </p>
-      <p>
-      Contamos con una vasta experiencia de más de 25 años y un equipo especializado, altamente comprometido con el desarrollo de procesos de mejora continua, garantizando el cumplimiento de los objetivos con altos estándares de calidad, seguridad y cuidando el medio ambiente.
-      </p>
+        <p>
+          En Exploservice ADL nos dedicamos a brindar soluciones en el campo de la perforación para voladuras, aplicadas para la exploración y explotación en minería y excavaciones para construcción.
+        </p>
+        <p>
+          Contamos con una vasta experiencia de más de 25 años y un equipo especializado, altamente comprometido con el desarrollo de procesos de mejora continua, garantizando el cumplimiento de los objetivos con altos estándares de calidad, seguridad y cuidando el medio ambiente.
+        </p>
+      </div>
       <h2>Nuestro servicios</h2>
       <div className={styles.serviceContainer}>
         <h2>PERFORACIÓN PARA PRODUCCION</h2>
