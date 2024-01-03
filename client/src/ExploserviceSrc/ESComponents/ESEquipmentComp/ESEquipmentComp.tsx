39e5aaf098beb61b0529701c396546be03cb99bd
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-03 17:23:43 -0300

    equipment styles

--- client/src/ExploserviceSrc/ESComponents/ESEquipmentComp/ESEquipmentComp.tsx
+++ client/src/ExploserviceSrc/ESComponents/ESEquipmentComp/ESEquipmentComp.tsx
@@ -11,22 +11,22 @@ const ESEquipmentComp = () => {
         <div className={styles.equipmentContainer}>
           <img src={frd} alt="FRD-image" />
           <div className={styles.types}>
-            <h3>1 x Furukawa HCR 1200 ED</h3>
-            <h3>5 x Furukawa HCR 1200 EDII</h3>
-            <h3>2 x Furukawa HCR 1500 EDII</h3>
+            <p>1 x Furukawa HCR 1200 ED</p>
+            <p>5 x Furukawa HCR 1200 EDII</p>
+            <p>2 x Furukawa HCR 1500 EDII</p>
           </div>
         </div>
         <div className={styles.equipmentContainer}>
           <img src={atlas} alt="FRD-image" />
           <div className={styles.types}>
-            <h3>1 x Atlas Copci Roc F9</h3>
-            <h3>1 x Atlas Copco Roc L8 25</h3>
+            <p>1 x Atlas Copci Roc F9</p>
+            <p>1 x Atlas Copco Roc L8 25</p>
           </div>
         </div>
         <div className={styles.equipmentContainer}>
           <img src={tamrock} alt="Tamrock-image" />
           <div className={styles.types}>
-            <h3>2 x Tamrock Ranger 800-2</h3>
+            <p>2 x Tamrock Ranger 800-2</p>
           </div>
         </div>
       </div>
