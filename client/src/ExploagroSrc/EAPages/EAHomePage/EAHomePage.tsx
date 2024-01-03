b114190134797faf372628414c1979dec143e867
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-03 16:05:32 -0300

    Styles

--- client/src/ExploagroSrc/EAPages/EAHomePage/EAHomePage.tsx
+++ client/src/ExploagroSrc/EAPages/EAHomePage/EAHomePage.tsx
@@ -1,6 +1,6 @@
 import EANavBarLayout from "../../EALayouts/EANavBarLayout/EANavBarLayout.tsx";
 import EAHomeImgComp from "../../EAComponents/EAHomeImgComp/EAHomeImgComp.tsx";
-import EAHomeTextComp from "../../EAComponents/EAHomeText/EAHomeTextComp.tsx";
+import EAHomeTextComp from "../../EAComponents/EAHomeTextComp/EAHomeTextComp.tsx";
 import EAHomeComp from "../../EAComponents/EAHomeComp/EAHomeComp..tsx";
 import EAFooterComp from "../../EAComponents/EAFooterComp/EAFooterComp.tsx";
 
