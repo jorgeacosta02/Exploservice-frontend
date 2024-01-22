1b65b77bb0ed444d793372d8c22436f7ccd94165
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-21 22:06:55 -0300

    contacPage relocation

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import { Routes, Route } from 'react-router-dom';
 import LandingPage from './GlobalPages/LandingPage';
-import ESCompanyPage from './ExploserviceSrc/ESPages/ESCompanyPage/ESCompanyPage';
+import ESCompanyPage from './pages/companyPage/companyPage';
 import ESServicesPage from './ExploserviceSrc/ESPages/ESServicesPage/ESServicesPage';
 import EAHomePage from './ExploagroSrc/EAPages/EAHomePage/EAHomePage';
 import EAProductPage from './ExploagroSrc/EAPages/EAProductPage/EAProductPage';
 import EAContactPage from './ExploagroSrc/EAPages/EAContactPage/EAContactPage';
 import ESEquipmentPage from './ExploserviceSrc/ESPages/ESEquipmentPage/ESEquipmentPage';
 import ESJobsPage from './ExploserviceSrc/ESPages/ESJobsPage/ESJobsPage';
-import ESContactPage from './ExploserviceSrc/ESPages/ESContactPage/ESContactPage';
+import ESContactPage from './pages/contactPage/contactPage';
 
 const App = () => {
   return (
@@ -18,11 +18,11 @@ const App = () => {
         <Route path="/" element={<LandingPage />} />
 
         {/*Exploservice Routes*/}
-        <Route path="/exploservice/company" element={<ESCompanyPage />} />
-        <Route path="/exploservice/services" element={<ESServicesPage />} />
-        <Route path="/exploservice/equipment" element={<ESEquipmentPage />} />
-        <Route path="/exploservice/works" element={<ESJobsPage />} />
-        <Route path="/exploservice/contact" element={<ESContactPage />} />
+        <Route path="/company" element={<ESCompanyPage />} />
+        <Route path="/services" element={<ESServicesPage />} />
+        <Route path="/equipment" element={<ESEquipmentPage />} />
+        <Route path="/works" element={<ESJobsPage />} />
+        <Route path="/contact" element={<ESContactPage />} />
   
         {/*Exploagro Routes*/}
         <Route path="/exploagro/" element={<EAHomePage />} />
