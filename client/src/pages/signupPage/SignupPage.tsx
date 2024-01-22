b981026ccfb72a71cb38bde49298b12390229d13
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-01-22 12:20:44 -0300

    watch values

--- client/src/pages/signupPage/SignupPage.tsx
+++ client/src/pages/signupPage/SignupPage.tsx
@@ -8,11 +8,19 @@ interface FormData {
 }
 
 const SignupPage = () => {
-  const { register, handleSubmit, formState: { errors } } = useForm<FormData>();
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<FormData>();
 
   const onSubmit: SubmitHandler<FormData> = (data) => {
     console.log(data);
   };
+  
+  const firstName = watch('firstName');
+  const lastName = watch('lastName');
+  const dni = watch('dni');
+
+  console.log('Watched Fields:', { firstName, lastName, dni });
+
+
   return (
     <div className={styles.container}>
       <form
@@ -22,21 +30,32 @@ const SignupPage = () => {
           Registrar persona
         </h1>
         <div className={styles.inputBlock}>
-          <label htmlFor='firstName'>Nombre</label>
-          <input {...register('firstName', { required: 'El nombre es requerido' })} />
+          {/* <label htmlFor='firstName'>Nombre</label> */}
+          <input {...register(
+              'firstName',
+              { required: 'El nombre es requerido' })}
+              placeholder='Ingrese nombre...'
+          />
         {errors.firstName && <span>{errors.firstName.message}</span>}
         </div>
         <div className={styles.inputBlock}>
-          <label htmlFor='lastName'>Apellido</label>
-          <input {...register('lastName', { required: 'El apellido es requerido' })} />
+          {/* <label htmlFor='lastName'>Apellido</label> */}
+          <input {...register(
+            'lastName',
+            { required: 'El apellido es requerido' })}
+            placeholder='Ingrese apellido...'
+          />
         {errors.lastName && <span>{errors.lastName.message}</span>}
         </div>
         <div className={styles.inputBlock}>
-          <label htmlFor='firstName'>dni</label>
-          <input {...register('dni', { required: 'El dni es requerido' })} />
+          {/* <label htmlFor='firstName'>dni</label> */}
+          <input {...register(
+            'dni',
+            { required: 'El dni es requerido' })}
+            placeholder='Ingrese dni...'
+          />
         {errors.dni && <span>{errors.dni.message}</span>}
         </div>
-       
       </form>
     </div>
   )
