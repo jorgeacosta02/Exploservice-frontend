f57ee06f02974865bfc00680483edaf46fcd0d5b
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-03-07 21:04:56 -0300

    filters in inquiryComp

--- src/components/inquiryComp/InquiryComp.tsx
+++ src/components/inquiryComp/InquiryComp.tsx
@@ -1,13 +1,7 @@
 import styles from './_InquiryComp.module.scss';
 import { useState, useEffect } from 'react';
-import axios from 'axios'
 import { useSelector, useDispatch } from 'react-redux';
-// import MessageComp from '../messageComp/MessageComp';
-import { IInvMovData } from '../../Interfaces/invMovInterfaces';
-import { selectLangState } from '../../redux/slices/langSlice';
-import { 
-  // selectMessageState, 
-  toggleMessage } from '../../redux/slices/messageSlice';
+// import { selectLangState } from '../../redux/slices/langSlice';
 import { getAllInquiriesAction } from '../../redux/actions/inquiryActions';
 import { selectInquiryState } from '../../redux/slices/inquirySlice';
 import InqSingleComp from './InqSingleComp';
@@ -16,177 +10,33 @@ import InqSingleComp from './InqSingleComp';
 const InquiryComp = () => {
 
    // Estados globales para opciones
-  const langState = useSelector(selectLangState);
-  // const messageState = useSelector(selectMessageState).message;
+  // const langState = useSelector(selectLangState);
   const inquiryState = useSelector(selectInquiryState).data
 
   const dispatch = useDispatch();
 
-  // const movementTypes = ['entrada', 'salida', 'transferencia']
-  
+  const [options, setOptions] = useState({
+    article:'Barra de perforación',
+    feature1:'T38',
+    feature2:'',
+    brand:'',
+    location:'Tocota',
+    quantity:''
+  })
+
+  // let filterOptions = {
+  //   article: options.article &&
+  // }
+
   const start = async () =>{
     await dispatch(getAllInquiriesAction());
   }
 
- 
   useEffect(()=>{
     start()
   },[]);
   
   console.log('inquiryState: ', inquiryState);
-  // console.log('selectArticlesState: ', selectArticleState)
-  // console.log('articleState: ',articleState);
-  // console.log('selectLangState: ', selectLangState)
-  // console.log('langState: ',langState);
-   
-  // Estado de datos del formulario
-  const [formData, setFormData] = useState<IInvMovData>({
-    movementType: '',
-    articleId: '',
-    originLocationId: '',
-    destinationLocationId: '',
-    quantity: 0
-  });
- 
-   // Estado de errores del formulario
-   const [errors, setErrors] = useState<IInvMovData>({
-    movementType: '',
-    articleId: '',
-    originLocationId: '',
-    destinationLocationId: '',
-    quantity: 0
-   });
-   
-   // Comprobación de estados para enviar formulario
-   let submitOk = false;
-  
-  if(
-    formData.movementType !== '' &&
-    formData.articleId !== '' &&
-    formData.originLocationId !== '' &&
-    formData.destinationLocationId !== '' &&
-    formData.quantity !== 0
-  ){
-    submitOk = true;
-  };
-   
-   // Expresiones de validación
-  //  const nameRegExp = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]*$/;
-  //  const emailRegExp = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/;
- 
-   const handleInputChange = (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => {
-     const { name, value } = e.target;
-         setFormData((prevData) => ({
-           ...prevData,
-           [name]: value,
-         }));
-         setErrors((prevData) => ({
-           ...prevData,
-           [name]: '',
-         }));
-   }
- 
-   const emptyMessage = langState === 'es' ?
-     'Este campo debe ser completado.' :
-     'This field must be filled out.';
- 
-   const emptyValidationHandler =()=>{
-     if(!formData.movementType){
-       setErrors((prevData) => ({
-         ...prevData,
-         movementType: emptyMessage,
-       }));
-     };
-     if(!formData.articleId){
-       setErrors((prevData) => ({
-         ...prevData,
-         articleId: emptyMessage,
-       }));
-     };
-     if(!formData.originLocationId){
-       setErrors((prevData) => ({
-         ...prevData,
-         originLocationId: emptyMessage,
-       }));
-     };
-     if(!formData.destinationLocationId){
-       setErrors((prevData) => ({
-         ...prevData,
-         destinationLocationId: emptyMessage,
-       }));
-     };
-     if(formData.quantity <= 0){
-       setErrors((prevData) => ({
-         ...prevData,
-         quantity: emptyMessage,
-       }));
-     };
-   };
-   
-   
-   const handleSubmit = (event:any) => {
-     event.preventDefault();
-     console.log('submit')
-     if(!submitOk) return emptyValidationHandler();
-     submitForm();
-   }
-   
-   const messageHandleClick = () => {
-     dispatch( toggleMessage() );
-   };
- 
-   const submitForm = async () => {
-     try{
-       const response = await axios.post(
-         'http://localhost:5000/inventory',
-          formData
-        );
-        console.log('response', response.status);
-        // queryResponse = await response.status;
-        setFormData({
-          movementType: '',
-          articleId:'',
-          originLocationId:'',
-          destinationLocationId:'',
-          quantity: 0
-        })
-        messageHandleClick()
-     }catch(error:any){
-       console.log(error.message)
-     }
-   }
-
-   const addGenreHandler =(event:any)=>{
-        
-    const selArticle = event.target.value;
-
-    console.log('selArticle: ', selArticle)
-
-    const { name, value } = event.target;
-    setFormData((prevData) => ({
-      ...prevData,
-      [name]: value,
-    }));
-    setErrors((prevData) => ({
-      ...prevData,
-      [name]: '',
-    }));
-
-    // const selGenreName = event.target.options[event.target.selectedIndex].label;
-    
-    // if(!gameContent.genreIds.includes(selGenre)){
-    //     setGameContent({...gameContent,genreIds:[...gameContent.genreIds,selGenre]})
-    //     setErrorGenreIds('');
-    //     const data = {id:selGenre, name:selGenreName}
-    //     return dispatch(addIdNameGenre(data))
-    // };
-    
-    // console.log('gameContent.genreIds al final dentro de genresHandler', gameContent.genreIds);
-  };
-
-  // console.log('formData :', formData)
-  // console.log('errors :', errors)
-
   
   return (
     <div className={styles.container}>
@@ -194,23 +44,29 @@ const InquiryComp = () => {
         <h1 className={styles.title}>
           Consultas
         </h1>
-        {/* <p>{inquiryState[0]?.article?.brand}</p> */}
         {
-          inquiryState.map((inq:any) => (
-            <div>
-              {/* <p>Uno</p> */}
-              <InqSingleComp
-                key={inq.id}
-                article={inq.article.name}
-                feature1={inq.article.feature1}
-                feature2={inq.article.feature2}
-                brand={inq.article.brand}
-                location={inq.location.name}
-                quantity={inq.amount}
-              />
-            </div>
-          ))
+          inquiryState
+          .filter((inq:any) => 
+          (!options.article || inq.article.name === options.article) && 
+          (!options.feature1 || inq.article.feature1 === options.feature1) && 
+          (!options.feature2 || inq.article.feature1 === options.feature2) && 
+          (!options.brand || inq.article.brand === options.brand) && 
+          (!options.location || inq.location.name === options.location)
+        )
+            .map((inq:any) => (
+              <div key={inq.id}>
+                <InqSingleComp
+                  article={inq.article.name}
+                  feature1={inq.article.feature1}
+                  feature2={inq.article.feature2}
+                  brand={inq.article.brand}
+                  location={inq.location.name}
+                  quantity={inq.amount}
+                />
+              </div>
+            ))
         }
+
       </div>
     </div>
   )
