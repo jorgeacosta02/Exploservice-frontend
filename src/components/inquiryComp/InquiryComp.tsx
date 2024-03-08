ad4a97cf34d7d3edbc5d40527991bad242c263f8
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-03-08 11:34:50 -0300

    cleanfilters button in inquiryComp

--- src/components/inquiryComp/InquiryComp.tsx
+++ src/components/inquiryComp/InquiryComp.tsx
@@ -7,10 +7,11 @@ import { selectInquiryState } from '../../redux/slices/inquirySlice';
 import InqSingleComp from './InqSingleComp';
 import { selectArticleState } from '../../redux/slices/articleSlice';
 import { getAllArticlesAction } from '../../redux/actions/articleActions';
-// import { selectLocationState } from '../../redux/slices/locationSlice';
+import { IArticleFormDB } from '../../Interfaces/articleInterfaces';
 import { getAllLocationsAction } from '../../redux/actions/locationActions';
 import { selectLangState } from '../../redux/slices/langSlice';
 import { Link } from 'react-router-dom';
+
 
 
 const InquiryComp = () => {
@@ -50,94 +51,108 @@ const InquiryComp = () => {
     }));
   };
 
+  // Obtener una lista de nombres únicos de los artículos
+  const uniqueArticleNames: any[] = Array.from(new Set<IArticleFormDB>(articleState.map((art:any) => art.name)));
+
   //Array filtrado por article para mostrar options de feature1
   let brandOptions = articleState
                 .filter((art:any) => 
                   (!options.article || art.name === options.article)
                 )
   console.log('brandOptions: ', brandOptions);
+
+  const cleanFiltersHandler = () => {
+    setOptions({
+      article:'',
+      brand:'',
+      group1:'',
+      group2:'',
+      location:'',
+      quantity:''
+    })
+  }
   
   return (
     <div className={styles.container}>
-      <div className={styles.formContainer}>
-        <h1 className={styles.title}>
-          Consultas
-        </h1>
-        <div>
-          <div className={styles.inputBlock}>
-            <label 
-              htmlFor='article'>
-              {langState === 'es' ? 'Artículo' : 'Article'}
-            </label>
-            <select
-                className={styles.select}
-                id="article"
-                name="article"
-                value={options.article}
-                onChange={handleChange}
-            >
-              <option value="" disabled selected>
-                Seleccionar un artículo
+      <h1 className={styles.title}>
+        Consultas
+      </h1>
+      <div className={styles.selectGroup}>
+        <button
+          onClick={cleanFiltersHandler}
+        >
+          Limpiar filtros
+        </button>
+        <div className={styles.selectBlock}>
+          <label 
+            htmlFor='article'>
+            {langState === 'es' ? 'Artículo' : 'Article'}
+          </label>
+          <select
+              className={styles.select}
+              id="article"
+              name="article"
+              value={options.article}
+              onChange={handleChange}
+          >
+            <option value="" disabled selected>
+              Seleccionar un artículo
+            </option>
+            {uniqueArticleNames.map((name, index) => (
+              <option key={index} value={name}>
+                {name}
               </option>
-              {articleState.map((art:any) => (
-                  <option
-                  key={art.id}
-                  value={art.name}
-              >
-                  {art.name}
-              </option>
-              ))}
-            </select>
-          </div>
-          <div className={styles.inputBlock}>
-            <label 
-              htmlFor='brand'>
-              {langState === 'es' ? 'Marca' : 'Brand'}
-            </label>
-            <select
-                className={styles.select}
-                id="brand"
-                name="brand"
-                value={options.brand}
-                onChange={handleChange}
-            >
-              <option value="" disabled selected>
-                Seleccionar característica 1
-              </option>
-              {brandOptions.map((art:any) => (
-                    <option
-                    key={art.id}
-                    value={art.brand}
-              >
-                  {art.brand}
-              </option>
-              ))}
-            </select>
-          </div>
+            ))}
+          </select>
         </div>
-        {
-          inquiryState
-          .filter((inq:any) => 
-          (!options.article || inq.article.name === options.article) && 
-          (!options.brand || inq.article.brand === options.brand) && 
-          (!options.group1 || inq.article.group1 === options.group1) && 
-          (!options.group2 || inq.article.group1 === options.group2) && 
-          (!options.location || inq.location.name === options.location)
-        )
-          .map((inq:any) => (
-            <div key={inq.id}>
-              <InqSingleComp
-                article={inq.article.name}
-                brand={inq.article.brand}
-                feature1={inq.article.group1}
-                feature2={inq.article.group2}
-                location={inq.location.name}
-                quantity={inq.amount}
-              />
-            </div>
-          ))
-        }
+        <div className={styles.selectBlock}>
+          <label 
+            htmlFor='brand'>
+            {langState === 'es' ? 'Marca' : 'Brand'}
+          </label>
+          <select
+              className={styles.select}
+              id="brand"
+              name="brand"
+              value={options.brand}
+              onChange={handleChange}
+          >
+            <option value="" disabled selected>
+              Seleccionar Marca
+            </option>
+            {brandOptions.map((art:any) => (
+              <option
+                key={art.id}
+                value={art.brand}
+              >
+                {art.brand}
+              </option>
+            ))}
+          </select>
+        </div>
       </div>
+      {
+        inquiryState
+        .filter((inq:any) => 
+        (!options.article || inq.article.name === options.article) && 
+        (!options.brand || inq.article.brand === options.brand) && 
+        (!options.group1 || inq.article.group1 === options.group1) && 
+        (!options.group2 || inq.article.group1 === options.group2) && 
+        (!options.location || inq.location.name === options.location)
+      )
+        .map((inq:any) => (
+          <div key={inq.id}>
+            <InqSingleComp
+              article={inq.article.name}
+              brand={inq.article.brand}
+              feature1={inq.article.group1}
+              feature2={inq.article.group2}
+              location={inq.location.name}
+              quantity={inq.amount}
+            />
+          </div>
+        ))
+      }
       <Link to='/inventory-movement'>Inventory Movemente</Link>
     </div>
   )
