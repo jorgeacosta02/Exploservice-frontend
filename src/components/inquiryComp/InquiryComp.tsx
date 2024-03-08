e4026a74fcd8f4015ccabd3c7598324067acc2cb
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-03-07 21:21:49 -0300

    select in inquiryComp

--- src/components/inquiryComp/InquiryComp.tsx
+++ src/components/inquiryComp/InquiryComp.tsx
@@ -5,31 +5,34 @@ import { useSelector, useDispatch } from 'react-redux';
 import { getAllInquiriesAction } from '../../redux/actions/inquiryActions';
 import { selectInquiryState } from '../../redux/slices/inquirySlice';
 import InqSingleComp from './InqSingleComp';
+import { selectArticleState } from '../../redux/slices/articleSlice';
+import { getAllArticlesAction } from '../../redux/actions/articleActions';
+import { selectLocationState } from '../../redux/slices/locationSlice';
+import { getAllLocationsAction } from '../../redux/actions/locationActions';
+import { selectLangState } from '../../redux/slices/langSlice';
 
 
 const InquiryComp = () => {
 
-   // Estados globales para opciones
-  // const langState = useSelector(selectLangState);
+  const langState = useSelector(selectLangState).lang
+  const articleState = useSelector(selectArticleState).data
   const inquiryState = useSelector(selectInquiryState).data
 
   const dispatch = useDispatch();
 
   const [options, setOptions] = useState({
-    article:'Barra de perforación',
-    feature1:'T38',
+    article:'',
+    feature1:'',
     feature2:'',
     brand:'',
-    location:'Tocota',
+    location:'Central',
     quantity:''
   })
 
-  // let filterOptions = {
-  //   article: options.article &&
-  // }
-
   const start = async () =>{
     await dispatch(getAllInquiriesAction());
+    await dispatch(getAllArticlesAction());
+    await dispatch(getAllLocationsAction());
   }
 
   useEffect(()=>{
@@ -44,6 +47,33 @@ const InquiryComp = () => {
         <h1 className={styles.title}>
           Consultas
         </h1>
+        <div>
+        <div className={styles.inputBlock}>
+            <label 
+              htmlFor='articleId'>
+              {langState === 'es' ? 'Nombre' : 'Name'}
+            </label>
+            <select
+                className={styles.select}
+                id="articleId"
+                name="articleId"
+                // value={formData.articleId}
+                // onChange={addGenreHandler}
+            >
+              <option value="" disabled selected>
+                Seleccionar un artículo
+              </option>
+              {articleState.map((art:any) => (
+                  <option
+                  key={art.id}
+                  value={art.id}
+              >
+                  {art.name}
+              </option>
+              ))}
+            </select>
+          </div>
+        </div>
         {
           inquiryState
           .filter((inq:any) => 
@@ -53,20 +83,19 @@ const InquiryComp = () => {
           (!options.brand || inq.article.brand === options.brand) && 
           (!options.location || inq.location.name === options.location)
         )
-            .map((inq:any) => (
-              <div key={inq.id}>
-                <InqSingleComp
-                  article={inq.article.name}
-                  feature1={inq.article.feature1}
-                  feature2={inq.article.feature2}
-                  brand={inq.article.brand}
-                  location={inq.location.name}
-                  quantity={inq.amount}
-                />
-              </div>
-            ))
+          .map((inq:any) => (
+            <div key={inq.id}>
+              <InqSingleComp
+                article={inq.article.name}
+                feature1={inq.article.feature1}
+                feature2={inq.article.feature2}
+                brand={inq.article.brand}
+                location={inq.location.name}
+                quantity={inq.amount}
+              />
+            </div>
+          ))
         }
-
       </div>
     </div>
   )
--- src/redux/actions/locationActions.ts
+++ src/redux/actions/locationActions.ts
@@ -6,9 +6,9 @@ import axios from 'axios';
 export const getAllLocationsAction: any = createAsyncThunk('http://localhost:5000/location', async () => {
   try {
     const response = await axios.get('http://localhost:5000/location');
-    console.log('response.data en getAllLocationsAction: ', response.data);
+    // console.log('response.data en getAllLocationsAction: ', response.data);
     const data = response.data;
-    console.log('data: ', data)
+    // console.log('data: ', data)
     return data;
   } catch (error) {
     throw new Error('Error en la solicitud de inicio de sesión');
