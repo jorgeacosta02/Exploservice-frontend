fd48ff25556d43030df916b12ee9510ca3009157
jorgeacosta02 <jorgeacostadeleon11@yahoo.com>
2024-03-15 12:43:24 -0300

    styles

--- src/components/intranet/IntranetComp.tsx
+++ src/components/intranet/IntranetComp.tsx
@@ -4,63 +4,60 @@ import { NavLink } from 'react-router-dom'
 const IntranetComp = () => {
   return (
     <div className={styles.container}>
-        <h1 className={styles.mainTitle}>
+        <div className={styles.backImgContainer}></div>
+        <div className={styles.backImgCover}></div>
+        <h3 className={styles.mainTitle}>
             Intranet
-        </h1>
+        </h3>
         <div className={styles.dataContainer}>
-            <h3 className={styles.blockTitle}>
+            <h4 className={styles.blockTitle}>
                 Generar formulario de:
-            </h3>
-            <ul className={styles.linksContainer}>
-                <li>
+            </h4>
+            <div className={styles.linksContainer}>
+
                     <NavLink 
                         to='/article-up-form'
                         className={styles.link}
                     >
                         Alta de artículo en almacenes
                     </NavLink>
-                </li>
-                <li>
+   
                     <NavLink 
                         to='/location-up-form'
                         className={styles.link}
                     >
                         Alta de location en almacenes
                     </NavLink>
-                </li>
-                <li>
+    
                     <NavLink 
                         to='/inventory-movement'
                         className={styles.link}
                     >
                         Movimiento en almacenes
                     </NavLink>
-                </li>
-                <li>
+
                     <NavLink 
                         to='/inquiry'
                         className={styles.link}
                     >
                         Consultas
                     </NavLink>
-                </li>
-                <li>
+
                     <NavLink 
                         to='#'
                         className={styles.link}
                     >
                         Reparación de veículos y equips
                     </NavLink>
-                </li>
-                <li>
+
                     <NavLink 
                         to='#'
                         className={styles.link}
                     >
                         Mantenimiento de instalaciones
                     </NavLink>
-                </li>
-            </ul>
+
+            </div>
         </div>
     </div>
   )
